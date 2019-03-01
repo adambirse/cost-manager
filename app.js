04d2d41e09358cc564e05d34d0620186d6a7bafb
adambirse <adambirse@gmail.com>
2019-03-01 14:22:22 +0000

    kafka support added.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +23,47 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient({kafkaHost: 'kafka1:9092,kafka2:9092,kafka3:9092'}),
+    consumer = new Consumer(
+        client,
+        [{topic: 'cost'}],
+        {
+            groupId: 'cost-manager',
+            autoCommit: false,
+            fromOffset: true
+        }
+    );
+
+
+consumer.on('message', function (message) {
+    console.log(message);
+});
+
+consumer.on('error', function (err) {
+    console.log('Error:', err);
+});
+
+consumer.on('offsetOutOfRange', function (err) {
+    console.log('offsetOutOfRange:', err);
+});
+
 
 module.exports = app;
