bf34722a1501947d68b513baa0f549cbcdc18893
adambirse <adambirse@gmail.com>
2019-03-04 13:25:13 +0000

    fixed bug

--- routes/costs.js
+++ routes/costs.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var title = 'Costs';
+var costs = [];
 /* GET costs listing. */
 router.get('/', function(req, res, next) {
   res.render('costs', { title: title, costs: costs });
