a91157aad6705a1e02631d9a63204725ad897539
adambirse <adambirse@gmail.com>
2019-04-05 14:48:40 +0100

    Bootstrap and minimal styling added.

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect
+
 app.use('/', indexRouter);
 app.use('/costs', costsRouter.router);
 
